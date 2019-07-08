7533f45c83bc9d6aef2e6edb198d3dfeb410f653
Tadeusz <tadolak@gmail.com>
2019-07-08 20:33:38 +0200

    prepare for AsyncWorker

--- app/src/main/java/com/tadolak/awariedb/MainActivity.java
+++ app/src/main/java/com/tadolak/awariedb/MainActivity.java
@@ -2,6 +2,7 @@ package com.tadolak.awariedb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UserNameEt = (EditText)findViewById(R.id.edName);
+        PasswordEt = (EditText)findViewById(R.id.etPassword);
+
+    }
+
+    public void OnLogin(View view){
+        String userName = UserNameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+        String type = "Login";
+
+        AsyncWorker asyncWorker = new AsyncWorker(this);
+        asyncWorker.execute(type, userName, password);
     }
 }
