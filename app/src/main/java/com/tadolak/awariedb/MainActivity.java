fad7fbe55f0e0c411755aa1585b8529a09b1aaa2
Tadeusz <tadolak@gmail.com>
2019-07-08 19:43:51 +0200

    Masster

---
+++ app/src/main/java/com/tadolak/awariedb/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tadolak.awariedb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText UserNameEt;
+    EditText PasswordEt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
